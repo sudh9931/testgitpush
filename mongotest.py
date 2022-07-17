57668f2442418738467965853920f4eac040aef1
iNeuronai <sudhanshu@ineuron.ai>
2022-07-17 19:28:13 +0530

    this is my first code push in gitfasfas fd

--- mongotest.py
+++ mongotest.py
@@ -8,6 +8,32 @@ d = {
     "email" : "sudhanshu@ineuron.ai",
     "surname" : "kumar"
 }
+d = {
+    "name":"sudhanshu",
+    "email" : "sudhanshu@ineuron.ai",
+    "surname" : "kumar"
+}
+d = {
+    "name":"sudhanshu",
+    "email" : "sudhanshu@ineuron.ai",
+    "surname" : "kumar"
+}
+d = {
+    "name":"sudhanshu",
+    "email" : "sudhanshu@ineuron.ai",
+    "surname" : "kumar"
+}
+d = {
+    "name":"sudhanshu",
+    "email" : "sudhanshu@ineuron.ai",
+    "surname" : "kumar"
+}
+d = {
+    "name":"sudhanshu",
+    "email" : "sudhanshu@ineuron.ai",
+    "surname" : "kumar"
+}
+
 db1 = client['mongotest']
 coll = db1['test']
 coll.insert_one(d )
